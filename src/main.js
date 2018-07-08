8c961ea2671fcd3ee43728000695c7eb0aa35884
ys <272165251@qq.com>
2018-07-08 19:01:45 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/style.css';
+import MyAxios from '@/plugins/MyAxios';
 
+// 注册MyAxios插件
+Vue.use(MyAxios);
+// 注册ElementUI插件
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
+// 下面这个注释不能删除，作用是eslint执行时候忽略下面文件
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
